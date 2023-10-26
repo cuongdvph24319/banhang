9b05ba08e64a543a9bebf1ef249c9293552cdc71
Đ.V Cường <cuongdvph24319@fpt.edu.vn>
2023-10-26 10:51:16 +0700

    create_project

---
+++ src/main/java/com/example/banhang/BanhangApplication.java
@@ -0,0 +1,13 @@
+package com.example.banhang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BanhangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BanhangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/banhang/BanhangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.banhang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanhangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
